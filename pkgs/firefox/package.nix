59469395417646bb9f28b1e51260856e7db5be04
ionawr <git.shiul@passmail.net>
2025-08-25 15:20:37 +0900

    feat(firefox): append custom theme

--- pkgs/firefox/package.nix
+++ pkgs/firefox/package.nix
@@ -5,6 +5,7 @@
   yarnConfigHook,
   yarnBuildHook,
   nodejs,
+  jq,
 }:
 buildCatppuccinPort {
   port = "firefox";
@@ -21,13 +22,30 @@ buildCatppuccinPort {
   yarnBuildScript = "generate";
 
   installPhase = ''
+    set -euo pipefail
     mkdir -p $out
-    cp themes.json $out
+
+    jq -s '
+      def deepmerge(a; b):
+        if (a|type)=="object" and (b|type)=="object" then
+          reduce ((a|keys_unsorted) + (b|keys_unsorted) | unique)[] as $k
+            ({}; .[$k] = deepmerge(a[$k]; b[$k]))
+        elif (a|type)=="array" and (b|type)=="array" then
+          (a + b) | unique
+        else
+          b // a
+        end;
+
+      .[0] as $base | .[1] as $patch
+      | $base
+      | .mocha = deepmerge(.mocha // {}; $patch.mocha // {})
+    ' themes.json ${./custom-theme.json} > "$out/themes.json"
   '';
 
   nativeBuildInputs = [
     yarnConfigHook
     yarnBuildHook
     nodejs
+    jq
   ];
 }
